30f7f2c0f71ec73367fcc2c5acb9a8af964de16b
KieranDingwall <115724862+KieranDingwall@users.noreply.github.com>
2023-03-31 11:00:36 +0100

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,16 @@
 function send_form(){
     document.getElementById("form_outcome").innerHTML="Form has been sent successfully!"
-      
+}
+
+function send_form(){
+    document.getElementById("form_outcomeLogin")
+    
+        if(counter % 2 == 0){
+            alert('You have been logged in!');
+        
+        } else {
+            alert('You have been logged out!');
+        }
+    counter += 1;
+    
 }
