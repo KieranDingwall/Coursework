210b08b8213f9131ae35c3ef578bfd8e06e7fbc1
KieranDingwall <115724862+KieranDingwall@users.noreply.github.com>
2023-04-18 21:16:35 +0100

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,16 +1,78 @@
+// Contact Us Form sending message
+
 function send_form(){
     document.getElementById("form_outcome").innerHTML="Form has been sent successfully!"
 }
 
-function send_form(){
-    document.getElementById("form_outcomeLogin")
+// Login form validation
+
+function login(){
+    document.getElementById("loginform")
     
-        if(counter % 2 == 0){
-            alert('You have been logged in!');
-        
+    
+        if (counter % 2 == 0) {
+            if(document.getElementById("loginform").checkValidity()){
+                alert('You have been logged out!')
+            } else {
+                alert('Form Invalid')
+            }
         } else {
-            alert('You have been logged out!');
+            alert('You have been logged in!'); 
         }
     counter += 1;
+
+    
     
 }
+
+// Shop Filters Javascript
+
+function cancels(){
+    document.getElementById("adidas").hidden=false
+    document.getElementById("nike").hidden=false
+    document.getElementById("vans").hidden=false
+    document.getElementById("puma").hidden=false
+    document.getElementById("newbalance").hidden=false
+}
+
+function adidas(){
+    document.getElementById("adidas").hidden=false
+    document.getElementById("nike").hidden=true
+    document.getElementById("vans").hidden=true
+    document.getElementById("puma").hidden=true
+    document.getElementById("newbalance").hidden=true
+}
+
+function vans(){
+    document.getElementById("vans").hidden=false
+    document.getElementById("nike").hidden=true
+    document.getElementById("adidas").hidden=true
+    document.getElementById("puma").hidden=true
+    document.getElementById("newbalance").hidden=true
+}
+
+function nike(){
+    document.getElementById("nike").hidden=false
+    document.getElementById("adidas").hidden=true
+    document.getElementById("vans").hidden=true
+    document.getElementById("puma").hidden=true
+    document.getElementById("newbalance").hidden=true
+}
+
+function puma(){
+    document.getElementById("puma").hidden=false
+    document.getElementById("nike").hidden=true
+    document.getElementById("adidas").hidden=true
+    document.getElementById("vans").hidden=true
+    document.getElementById("newbalance").hidden=true
+}
+
+function newbalance(){
+    document.getElementById("newbalance").hidden=false
+    document.getElementById("nike").hidden=true
+    document.getElementById("adidas").hidden=true
+    document.getElementById("vans").hidden=true
+    document.getElementById("puma").hidden=true
+}
+
+
